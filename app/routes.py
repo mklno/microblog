a94feb7c2657ca89630a405a5694f30c2805be3c
mklno <manodreuel@gmail.com>
2023-12-06 16:07:08 +0530

    init commit

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {
+        'username' : 'Kafkaa'
+    }
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
+
+
+
